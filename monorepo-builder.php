1501560dec6adb66a3494ebe8bfe35bcd52497f6
Tomas Votruba <tomas.vot@gmail.com>
2022-03-07 16:27:52 +0100

    fix missing package name

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
