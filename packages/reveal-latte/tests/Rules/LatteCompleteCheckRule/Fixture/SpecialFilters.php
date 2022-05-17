d57c223213867874d6d0d5c8bd8bae829d35c82f
Michal Lulco <lulco@efabrica.sk>
2022-05-18 00:05:22 +0200

    Special filters

---
+++ packages/reveal-latte/tests/Rules/LatteCompleteCheckRule/Fixture/SpecialFilters.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Reveal\RevealLatte\Tests\Rules\LatteCompleteCheckRule\Fixture;
+
+use Nette\Application\UI\Control;
+use Nette\Application\UI\Form;
+
+final class SpecialFilters extends Control
+{
+    public function render(): void
+    {
+        $this->template->someString = 'foo bar';
+        $this->template->setFile(__DIR__ . '/../Source/SpecialFilters.latte');
+        $this->template->render();
+    }
+}
