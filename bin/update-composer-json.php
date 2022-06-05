1680d37160a2626e0a410206068371f754cde1d5
Tomas Votruba <tomas.vot@gmail.com>
2022-06-05 17:27:41 +0200

    bump 7.2

--- bin/update-composer-json.php
+++ bin/update-composer-json.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 // update composer.json version in file
 // before: "php": "^8.1"
-// after: "php": ">=7.1"
+// after: "php": ">=7.2"
 
 // usage: php bin/update-composer-json.php packages/phpstan-rules/composer.json
 
 $composerJsonFilePath = $argv[1];
 
 $composerJsonFile = file_get_contents($composerJsonFilePath);
-$composerJsonFile= str_replace('"php": ">=8.1"', '"php": ">=7.1"', $composerJsonFile);
+$composerJsonFile= str_replace('"php": ">=8.1"', '"php": ">=7.2"', $composerJsonFile);
 
 file_put_contents($composerJsonFilePath, $composerJsonFile);
 
-echo sprintf('PHP version in %s file was updated to 7.1' . PHP_EOL, $composerJsonFilePath);
+echo sprintf('PHP version in %s file was updated to 7.2' . PHP_EOL, $composerJsonFilePath);
