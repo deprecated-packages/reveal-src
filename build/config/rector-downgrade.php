86ff8923c29e3888c2ad496129d196ad2c46d5d8
Tomas Votruba <tomas.vot@gmail.com>
2022-06-05 17:21:43 +0200

    update

--- build/config/rector-downgrade.php
+++ build/config/rector-downgrade.php
@@ -3,17 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DowngradePhp74\Rector\ClassMethod\DowngradeCovariantReturnTypeRector;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->skip([
-        // should be skipped - fix in rectordev-main
-        DowngradeCovariantReturnTypeRector::class => [
-            'tests/Rule/ValueObjectDestructRule/ValueObjectDestructRuleTest.php'
-        ],
-    ]);
-
     $rectorConfig->sets([
         DowngradeLevelSetList::DOWN_TO_PHP_72,
     ]);
