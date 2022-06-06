9b02048bbf865072523754caf09a2d8cd6807b6d
Tomas Votruba <tomas.vot@gmail.com>
2022-06-06 18:25:45 +0200

    debug

--- scoper.php
+++ scoper.php
@@ -18,31 +18,31 @@ return [
     'exclude-namespaces' => ['#^PHPStan#', '#^Reveal#', '#^PhpParser#'],
     'expose-functions' => ['u', 'b', 's', 'trigger_deprecation'],
     'patchers' => [
-        // fixes https://github.com/rectorphp/rector/issues/7017
-//        function (string $filePath, string $prefix, string $content): string {
-//            if (str_ends_with($filePath, 'vendor/symfony/string/ByteString.php')) {
-//                return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
-//            }
-//
-//            if (str_ends_with($filePath, 'vendor/symfony/string/AbstractUnicodeString.php')) {
-//                return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
-//            }
-//
-//            return $content;
-//        },
+        // fixes https://github.com/rectorphp/rector/issues/7017 - still needed 2022-06
+        function (string $filePath, string $prefix, string $content): string {
+            if (str_ends_with($filePath, 'vendor/symfony/string/ByteString.php')) {
+                return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
+            }
+
+            if (str_ends_with($filePath, 'vendor/symfony/string/AbstractUnicodeString.php')) {
+                return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
+            }
+
+            return $content;
+        },
 
         // scoper missed PSR-4 autodiscovery in Symfony
-        function (string $filePath, string $prefix, string $content): string {
-            if (! \str_ends_with($filePath, 'config.php') && ! \str_ends_with($filePath, 'services.php')) {
-                return $content;
-            }
-
-            // skip "Rector\\" namespace
-            if (\str_contains($content, '$services->load(\'Reveal')) {
-                return $content;
-            }
-
-            return Strings::replace($content, '#services\->load\(\'#', "services->load('" . $prefix . '\\');
-        },
+//        function (string $filePath, string $prefix, string $content): string {
+//            if (! \str_ends_with($filePath, 'config.php') && ! \str_ends_with($filePath, 'services.php')) {
+//                return $content;
+//            }
+//
+//            // skip "Rector\\" namespace
+//            if (\str_contains($content, '$services->load(\'Reveal')) {
+//                return $content;
+//            }
+//
+//            return Strings::replace($content, '#services\->load\(\'#', "services->load('" . $prefix . '\\');
+//        },
     ],
 ];
