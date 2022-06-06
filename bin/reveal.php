b9e69ec7ddcb7a278679040d296debc4e66cb15f
Tomas Votruba <tomas.vot@gmail.com>
2022-06-06 16:47:26 +0200

    add scoper autoload

--- bin/reveal.php
+++ bin/reveal.php
@@ -7,6 +7,12 @@ use Symplify\SymplifyKernel\ValueObject\KernelBootAndApplicationRun;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// scoper autoload
+$scoperAutoloadFile = __DIR__ . '/../vendor/scoper-autoload.php';
+if ($scoperAutoloadFile) {
+    require_once $scoperAutoloadFile;
+}
+
 
 $kernelBootAndApplicationRun = new KernelBootAndApplicationRun(RevealKernel::class);
 $kernelBootAndApplicationRun->run();
