2502342644ef3393a306a51059c5c468228b5b6f
Michal Lulco <lulco@efabrica.sk>
2022-08-20 14:29:58 +0200

    [Scoper] Added namespaces to exclude (#30)

--- scoper.php
+++ scoper.php
@@ -15,16 +15,23 @@ $timestamp = $dateTime->format('Ymd');
 return [
     'prefix' => 'RevealPrefix' . $timestamp,
     // @see https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#exposed-symbols - regex format
-    'exclude-namespaces' => ['#^PHPStan#', '#^Reveal#', '#^PhpParser#'],
+    'exclude-namespaces' => [
+        '#^PHPStan#',
+        '#^Reveal#',
+        '#^PhpParser#',
+        '#^Symplify\\\\Astral#',
+        '#^Symplify\\\\SmartFileSystem#',
+        '#^Symplify\\\\PackageBuilder#',
+    ],
     'expose-functions' => ['u', 'b', 's', 'trigger_deprecation'],
     'patchers' => [
         // fixes https://github.com/rectorphp/rector/issues/7017 - still needed 2022-06
         function (string $filePath, string $prefix, string $content): string {
-            if (str_ends_with($filePath, 'vendor/symfony/string/ByteString.php')) {
+            if (\str_ends_with($filePath, 'vendor/symfony/string/ByteString.php')) {
                 return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
             }
 
-            if (str_ends_with($filePath, 'vendor/symfony/string/AbstractUnicodeString.php')) {
+            if (\str_ends_with($filePath, 'vendor/symfony/string/AbstractUnicodeString.php')) {
                 return Strings::replace($content, '#' . $prefix . '\\\\\\\\1_\\\\\\\\2#', '\\\\1_\\\\2');
             }
 
@@ -43,6 +50,15 @@ return [
             }
 
             return Strings::replace($content, '#services\->load\(\'#', "services->load('" . $prefix . '\\');
+        },
+
+        // latte services
+        function (string $filePath, string $prefix, string $content): string {
+            if (! \str_ends_with($filePath, 'config.neon') && ! \str_ends_with($filePath, 'services.neon')) {
+                return $content;
+            }
+
+            return Strings::replace($content, '#- Latte\\\\#', "- $prefix\\Latte\\");
         },
     ],
 ];
